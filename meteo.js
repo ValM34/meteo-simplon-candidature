5c6c95d5f4f6a945b77ecd7c0e947cb685ae781d
ValM34 <valentin.moreau34750@gmail.com>
2025-06-11 16:16:38 +0200

    first commit

---
+++ meteo.js
@@ -0,0 +1,107 @@
+async function init() {
+  handleMeteoCardState("loading");
+
+  // FETCH DATA
+  async function fetchTownName() {
+    const townName = await fetch(`./conf.json`)
+      .then((response) => response.json())
+      .then((data) => data.ville)
+      .catch((error) => {
+        console.error("Erreur interne :", error);
+      });
+
+    return townName;
+  }
+  let townName = await fetchTownName();
+  if(!townName) {
+    handleMeteoCardState("error");
+  }
+
+  async function fetchTownData() {
+    const townData = await fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${townName}&count=1&language=fr&format=json`)
+      .then((response) => response.json())
+      .then((data) => data.results[0])
+      .catch((error) => {
+        console.error("Erreur lors de la récupération des données de la ville :", error);
+      });
+
+    return townData;
+  }
+  let townData = await fetchTownData();
+  if(!townData) {
+    handleMeteoCardState("error");
+  }
+
+  async function fetchWeatherData() {
+    const weatherData = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${townData.latitude}&longitude=${townData.longitude}&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m,&models=meteofrance_seamless`)
+      .then((response) => response.json())
+      .then((data) => data)
+      .catch((error) => {
+        console.error("Erreur lors de la récupération des données météo :", error);
+      });
+
+    return weatherData;
+  }
+  let weatherData = await fetchWeatherData();
+  if(!weatherData || weatherData?.error === true) {
+    handleMeteoCardState("error");
+  } else {
+    handleMeteoCardState("success");
+  }
+
+  // DOM UPDATES
+  const date = new Date();
+  const hour = date.getHours();
+  const minutes = date.getMinutes();
+
+  document.querySelector('#town_name').textContent = townName;
+  document.querySelector('#town_date').textContent = date.toLocaleDateString();
+  document.querySelector('#town_hour').textContent = hour;
+  document.querySelector('#town_temperature').textContent = weatherData.hourly.temperature_2m[hour] + "°C";
+  document.querySelector('#town_humidity').textContent = weatherData.hourly.relative_humidity_2m[hour];
+  document.querySelector('#town_wind_speed').textContent = weatherData.hourly.wind_speed_10m[hour];
+
+  if(weatherData.hourly.temperature_2m[hour] > 20) {
+    document.querySelector('#town_temperature').style.color = "#f13e40";
+    document.querySelector('#town_temperature').style.background = "rgba(255, 0, 0, 0.1)";
+  } else {
+    document.querySelector('#town_temperature').style.color = "#008eff";
+    document.querySelector('#town_temperature').style.background = "rgba(0, 149, 255, 0.1)";
+  }
+
+  function handleMeteoCardState(state) {
+    if(state == "loading") {
+      document.querySelector('#meteo_card_loader').style.display = "flex";
+      document.querySelector('#meteo_card').style.display = "none";
+      document.querySelector('#error_message').style.display = "none";
+    } else if(state == "success") {
+      document.querySelector('#meteo_card_loader').style.display = "none";
+      document.querySelector('#meteo_card').style.display = "flex";
+      setTimeout(() => {
+        document.querySelector('#meteo_card').style.opacity = "1";
+      }, 100);
+      document.querySelector('#error_message').style.display = "none";
+    } else if(state == "error") {
+      document.querySelector('#meteo_card_loader').style.display = "none";
+      document.querySelector('#meteo_card').style.display = "none";
+      document.querySelector('#error_message').style.display = "block";
+    }
+  }
+}
+
+function getTimeUntilNextHour() {
+  const date = new Date();
+  const nextHourDate = new Date(date.getFullYear(), date.getMonth(), date.getDate(), date.getHours() + 1, 0, 0);
+  console.log(nextHourDate);
+  console.log(date);
+  return nextHourDate - date;
+}
+
+async function updateDataEachHour() {
+    console.log('Démarrage de la mise à jour horaire');
+    await init();
+    await new Promise(resolve => setTimeout(resolve, getTimeUntilNextHour()));
+    console.log('mise à jour terminee')
+}
+
+updateDataEachHour();
